9ed43e5f457ba6f561a81918ec4211ddc66f8f6d
hamidm317 <hamidm317@gmail.com>
2024-08-21 12:59:31 +0330

    Add new FunctionGenerators

--- Utils/FunctionGenerators.py
+++ Utils/FunctionGenerators.py
@@ -27,3 +27,43 @@ def SingleTone(sig_length, specs):
     else:
 
         return a * np.sin(2 * np.pi * time + phi)
+    
+def RandomlySmoothSpiking(sig_length, specs):
+
+    spike_rate = specs['spike_rate']
+    spike_amp = specs['spike_amp']
+    smooth_length = specs['smooth_length']
+
+    f_length = smooth_length + sig_length
+
+    NF_sig = MovMean(np.random.permutation(np.concatenate([np.zeros(int((1 - spike_rate) * f_length)), spike_amp * np.ones(int(spike_rate * f_length))])), smooth_length)
+
+    if specs['noisy']:
+
+        loc = specs['mean']
+        scale = specs['var']
+
+        return NF_sig + np.random.normal(loc = loc, scale = scale, size = sig_length)
+    
+    else:
+
+        return NF_sig
+
+def MovMean(A, k):
+
+    win_length = k
+    MAFA = []
+    
+    if len(A) < k:
+
+        print("I don't know how to calculate this, The window must be less than length. So the length forced to be len(A)")
+
+        win_length = len(A)
+
+    new_elements_len = len(A) - win_length + 1
+
+    for element in range(new_elements_len):
+
+        MAFA.append(np.mean(A[element : (element + 1) + k ]))
+
+    return np.array(MAFA)
